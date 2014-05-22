e7d082043c9c81d58658d91ce11f0d2754c5f375
IF <if.html@gmail.com>
2014-05-23 01:59:57 +0800

    merge

--- examples/21-deferred/deferred.cpp
+++ examples/21-deferred/deferred.cpp
@@ -223,7 +223,7 @@ int _main_(int /*_argc*/, char** /*_argv*/)
 {
 	uint32_t width = 1280;
 	uint32_t height = 720;
-	uint32_t debug = BGFX_DEBUG_TEXT;
+	uint32_t debug = BGFX_DEBUG_TEXT|BGFX_DEBUG_STATS;
 	uint32_t reset = BGFX_RESET_VSYNC;
 
 	bgfx::init();
