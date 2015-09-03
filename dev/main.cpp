026036e0f7fd048fbc5027ec2be746906cb279d5
if <if.html@gmail.com>
2015-09-03 18:06:56 +0800

    update bgfx

--- dev/main.cpp
+++ dev/main.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-void* loadfile(char const* filename)
+void* loadfile(char const* filename, size_t &fsize)
 {
     FILE* _file = fopen(filename, "rb");
     assert(_file);
@@ -18,6 +18,7 @@ void* loadfile(char const* filename)
     assert(fread(data, 1, size, _file) == size);
     fclose(_file);
 
+    fsize = size;
     return data;
 }
 
@@ -26,31 +27,33 @@ int _main_(int, char**)
     uint32_t width = 800, height = 600;
     uint32_t debug = BGFX_DEBUG_TEXT | BGFX_DEBUG_STATS;
     uint32_t reset = BGFX_RESET_VSYNC | BGFX_RESET_MSAA_X8;
+    bool showStats = true;
 
     bgfx::init();
     bgfx::reset(width, height, reset);
 
     bgfx::setDebug(debug);
 
-    void* fontdata = loadfile("assets/font/visitor1.ttf");
-    imguiCreate(fontdata);
+    size_t sz = 0;
+    void* fontdata = loadfile("assets/font/droidsans.ttf", sz);
+    imguiCreate(fontdata, sz);
     free(fontdata);
 
     entry::MouseState mouseState;
     float rgb[3] = {0.3f, 0.3f, 0.3f};
-    bool colorwheelActivated = true;
+    bool colorwheelActivated = false;
     int32_t scrollArea = 0;
     while(!entry::processEvents(width, height, debug, reset, &mouseState)) {
         auto encodeColor = [&rgb]()->uint32_t{
             return uint32_t(rgb[0]*255)<<24|uint32_t(rgb[1]*255)<<16|uint32_t(rgb[2]*255)<<8|0xff;
         };
         bgfx::setViewClear(0,
-                BGFX_CLEAR_COLOR_BIT | BGFX_CLEAR_DEPTH_BIT,
+                BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH,
                 encodeColor(), 1.0f, 0);
 
         bgfx::setViewRect(0, 0, 0, width, height);
         
-        bgfx::submit(0);
+        bgfx::submit(0, BGFX_INVALID_HANDLE);
         bgfx::dbgTextPrintf(0, 1, 0x4f, "hello world");
         bgfx::dbgTextPrintf(0, 2, 0x5f, "simple bgfx program");
 
@@ -60,11 +63,17 @@ int _main_(int, char**)
             , 0
             , width
             , height);
-        imguiBeginScrollArea("Test", width-350, 50, 330, 400, &scrollArea);
+        imguiBeginScrollArea("Test", width-350, 50, 330, 500, &scrollArea);
         imguiSeparatorLine();
         imguiLabel("foobar");
         if(imguiButton("say hi"))
             fprintf(stdout, "hi there\n");
+        imguiBool("show stats", showStats);
+        uint32_t newDebug = BGFX_DEBUG_TEXT | (showStats ? BGFX_DEBUG_STATS : 0);
+        if(newDebug != debug) {
+            debug = newDebug;
+            bgfx::setDebug(debug);
+        }
         imguiColorWheel("bg color", rgb, colorwheelActivated);
         imguiSeparatorLine();
         if(imguiButton("quit"))
